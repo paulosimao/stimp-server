54f519a839844ea8a0a03b6e9b57198139fdfe27
Paulo Simão <paulosimao@gmail.com>
2015-12-26 00:53:31 -0200

    On the way to the first release - backup commit only, not functional yet.

---
+++ test/test.js
@@ -0,0 +1,17 @@
+/**
+ * Created by paulo.simao on 23/12/2015.
+ */
+var Server = new require('../index')();
+Server.start();
+
+//describe('Simple Server Test', function () {
+//    it('Simple Server Test', function (done) {
+//    });
+//});
+//server.stop();
+//var s = 'a\nb\nc\n\n\def\0';
+//console.log(s);
+//var sm = s.split('\0');
+//console.log(sm);
+//var sm1 = sm[0];
+//console.log(sm1.split('\n'));
